bc10585e2ebb12596bc12880ae3aca0f53062415
skhadk10 <rickeykhd@gmail.com>
2022-12-29 15:01:39 +1100

    Header navbar form is done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import {Routes, Route} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
-// import Header from './component/nav/Header.js';
-// import Login from './pages/auth/Login.js';
-// import Register from './pages/auth/Register.js';
-// import RegisterComplete from './pages/auth/RegisterComplete.js';
-// import Home from './pages/Home.js'
+import Header from './component/nav/Header.js';
+import Login from './pages/auth/Login.js';
+import Register from './pages/auth/Register.js';
+import RegisterComplete from './pages/auth/RegisterComplete.js';
+import Home from './pages/Home.js'
 function App() {
   return (
     <>
     {/* <Header/> */}
     <ToastContainer/>
     <Routes>
-      {/* <Route exact path="/" element={<Home/>} />
+      <Route exact path="/" element={<Home/>} />
       <Route exact path="/register" element={<Register/>} />
       <Route exact path="/register/complete" element={<RegisterComplete/>} /> 
-    <Route exact path="/login" element={<Login/>} /> */}
+    <Route exact path="/login" element={<Login/>} />
     </Routes>
     
     </>
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Menu } from "antd";
+import {
+  AppstoreOutlined,
+  SettingOutlined,
+  UserOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
+const { SubMenu } = Menu;
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [current, setCurrent] = useState("home");
 
-export default Header
+  const handleClick = (e) => {
+    // console.log(e.key);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu
+      mode="horizontal"
+      defaultSelectedKeys={["home"]}
+      style={{ display: "block" }}
+    >
+      <Menu.Item key="home" icon={<AppstoreOutlined />}>
+        <Link to="/"> Home</Link>
+      </Menu.Item>
+      <SubMenu icon={<SettingOutlined />} title="Username">
+        <Menu.Item key="two">Navigation Two</Menu.Item>
+        <Menu.Item key="three">Navigation Three</Menu.Item>
+      </SubMenu>
+      <Menu.Item
+        key="login"
+        icon={<UserAddOutlined />}
+        style={{
+          float: "right",
+        }}
+      >
+        <Link to="/login"> Login</Link>
+      </Menu.Item>
+      <Menu.Item
+        key="register"
+        icon={<UserOutlined />}
+        style={{
+          float: "right",
+        }}
+      >
+        <Link to="/register"> Register</Link>
+      </Menu.Item>
+    </Menu>
+  );
+};
+
+export default Header;
