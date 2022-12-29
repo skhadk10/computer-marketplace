7c19745d75ab9d65e2c4a66e1aa30fdc59ebd9a0
skhadk10 <rickeykhd@gmail.com>
2022-12-29 14:52:56 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+// import Header from './component/nav/Header.js';
+// import Login from './pages/auth/Login.js';
+// import Register from './pages/auth/Register.js';
+// import RegisterComplete from './pages/auth/RegisterComplete.js';
+// import Home from './pages/Home.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <Header/> */}
+    <ToastContainer/>
+    <Routes>
+      {/* <Route exact path="/" element={<Home/>} />
+      <Route exact path="/register" element={<Register/>} />
+      <Route exact path="/register/complete" element={<RegisterComplete/>} /> 
+    <Route exact path="/login" element={<Login/>} /> */}
+    </Routes>
+    
+    </>
+
   );
 }
 
